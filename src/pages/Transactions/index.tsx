145a64e7011b91f160f0cfd094daa2c73b69f093
Victor Oliveira Almeida <victor.almeida.ti.celular@gmail.com>
2025-02-24 08:42:01 -0300

    servico

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,38 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./style";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <SearchForm />
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighLight variant="income">R$ 1.200,00</PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2025</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighLight variant="outcome">- R$ 1.200,00</PriceHighLight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>10/04/2025</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
+
         </div>
     )
 }
